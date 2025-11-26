6188db50e3b67f3ec8353577f5d72e1a14561fb4
Stiliyan <stiliyan01@gmail.com>
2025-11-26 13:29:58 +0200

    Create Find user  by external id.

--- src/main/java/bg/tu_varna/sit/si/user/UserResource.java
+++ src/main/java/bg/tu_varna/sit/si/user/UserResource.java
@@ -1,11 +1,12 @@
 package bg.tu_varna.sit.si.user;
 
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.transaction.UserTransaction;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
@@ -16,7 +17,19 @@ import java.util.List;
 public class UserResource {
 
     @GET
-    public List<UserEntity> getUsers(){
+    public List<UserEntity> getUsers() {
         return UserEntity.listAll();
+    }
+
+    @GET
+    @Path("/{externalId}")
+    public Response getUser(@PathParam("externalId") String externalId) {
+        UserEntity user = UserEntity.find("externalUserId", externalId).firstResult();
+
+        if(user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(user).build();
     }
 }
