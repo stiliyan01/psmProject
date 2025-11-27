93782d471fa2173192e3efaa36460352a10ea1eb
Stiliyan <stiliyan01@gmail.com>
2025-11-27 12:29:36 +0200

    Add Transactional to post

--- src/main/java/bg/tu_varna/sit/si/user/UserResource.java
+++ src/main/java/bg/tu_varna/sit/si/user/UserResource.java
@@ -43,6 +43,7 @@ public class UserResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
     public Response createUser(UserCreateDTO dto) {
 
         UserEntity user = new UserEntity();
